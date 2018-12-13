6a8f10069892f90f20d1bf1ae554f32bb9416ac2
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-12-13 10:10:20 -0700

    homepage component and file added

--- App.js
+++ App.js
@@ -21,7 +21,6 @@ export default class App extends Component<Props> {
   render() {
     return (
 
-
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to spotCheck!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
