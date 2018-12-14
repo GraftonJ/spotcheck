f3b1061d786b178127a6c7fd0b304f385fbae0ea
Josh <grafton.josh@gmail.com>
2018-12-14 10:34:02 -0700

    changed app.js back to pull master.

--- App.js
+++ App.js
@@ -2,8 +2,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 // import HomePage from './components/Homepage/Homepage'
-//Testing DetailCard Component below
-import ResultCards from './components/ResultCards/ResultCards'
+
+
 
 import AppNavigator from './routes';
 import HomeSCR from './screens/HomeSCR'
@@ -35,8 +35,7 @@ export default class App extends Component<Props> {
   }
   render() {
     return (
-      <ResultCards />
-      //<AppNavigator />
+      <AppNavigator />
     );
   }
 }
