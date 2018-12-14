9dd7b5e2d57cc93c336c1160b1b251f143b8d4ac
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-12-13 17:03:49 -0700

    Login and homepage styling

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Homepage from './components/Homepage/Homepage'
 
+
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -34,7 +36,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Homepage location={this.state.location} updateLocation={this.updateLocation}/>
+      <Homepage />
       </View>
     );
   }
