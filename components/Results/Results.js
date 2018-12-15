262375bd8e7849a9f0af07536333b21f5aa16b7a
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-12-14 17:04:58 -0700

    Able to get data from API

--- components/Results/Results.js
+++ components/Results/Results.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import {
   StyleSheet,
@@ -12,16 +11,59 @@ import {
   Alert,
   TouchableOpacity} from 'react-native';
 
-  import store from '../../store';
+  // import store from '../../store';
 
 
-const Results = () => (
-  <SafeAreaView style={styles.container}>
 
-    <Text>Results for: {store.getState().searchFor}</Text>
 
-  </SafeAreaView>
-)
+  const config = {
+    headers: {'Authorization': 'Bearer VkRXEkxkuMiPqFY3xuJdHIMU3ggnwWrKaeCdL-cMm5Mh0q_b-OyMhmdZDMf8xSrbV0BPdAaPtu0aVY2vlHRCQ1JZzFl0N-ahFSjwDY16uAvQ0YviTfxrydO32n6dW3Yx'},
+    params: {
+      term: 'dog friendly restaurants',
+      location: 'Boulder, CO'
+    }
+  }
+
+  const API = 'https://api.yelp.com/v3/businesses/search'
+
+  const getResults = async () => {
+  const response = await fetch(`${API}?location=${config.params.location}`, config)
+  const json = await response.json()
+  console.log(json)
+  return json
+}
+
+
+export default class Results extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      results: []
+    }
+  }
+
+
+  async componentWillMount() {
+
+    const response = await fetch(`${API}`, config)
+    const json = await getResults()
+    this.setState({
+      results: json
+    })
+}
+
+render() {
+  return (
+    <Text>Results</Text>
+  )
+}
+
+  // <SafeAreaView style={styles.container}>
+  //
+  //   <Text>Results for: {store.getState().searchFor}</Text>
+  //
+  // </SafeAreaView>
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -30,5 +72,3 @@ const styles = StyleSheet.create({
     justifyContent:'center',
   },
 })
-
-export default Results
