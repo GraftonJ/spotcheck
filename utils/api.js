e86bcd04bf5b280751006883556fadbbfb734d72
Ed Phelps <edphelps1@gmail.com>
2018-12-16 06:01:52 -0700

    added lat lon search

--- utils/api.js
+++ utils/api.js
@@ -11,46 +11,48 @@ import {
   Alert,
   TouchableOpacity} from 'react-native';
 
-  const RADIUS = 1000;
+const RADIUS = 100;
 
-  import store from '../store';
+import store from '../store';
 
-  const config = {
-    headers: {'Authorization': 'Bearer VkRXEkxkuMiPqFY3xuJdHIMU3ggnwWrKaeCdL-cMm5Mh0q_b-OyMhmdZDMf8xSrbV0BPdAaPtu0aVY2vlHRCQ1JZzFl0N-ahFSjwDY16uAvQ0YviTfxrydO32n6dW3Yx'},
-    params: {
-      term: 'dog+friendly+restaurants',
-    }
+const config = {
+  headers: {'Authorization': 'Bearer VkRXEkxkuMiPqFY3xuJdHIMU3ggnwWrKaeCdL-cMm5Mh0q_b-OyMhmdZDMf8xSrbV0BPdAaPtu0aVY2vlHRCQ1JZzFl0N-ahFSjwDY16uAvQ0YviTfxrydO32n6dW3Yx'},
+  params: {
+    term: 'dog+friendly+restaurants',
   }
+}
 
-  const API = 'https://api.yelp.com/v3/businesses/search'
+const API = 'https://api.yelp.com/v3/businesses/search'
 
-  /* *********************************************************
-  getResults()
-  @param location (string) ex: "Boulder, CO"
-  @return array of Yelp businesses with lots of fields!
-  ************************************************************ */
-  export const getResults = async (location) => {
-    config.params.location = location
-    const response = await fetch(`${API}?term=${config.params.term}&location=${location}`, config)
-    const json = await response.json()
-    return json.businesses
-  }
+/* *********************************************************
+getResults()
+@param location (string) ex: "Boulder, CO"
+@return array of Yelp businesses with lots of fields!
+************************************************************ */
+export const getResults = async (location) => {
+  config.params.location = location;
+  console.log("api::getResults() for: ", location);
+  console.log("api::getResults() for: ", `${API}?term=${config.params.term}&location=${location}`);
+  const response = await fetch(`${API}?term=${config.params.term}&location=${location}`, config)
+  const json = await response.json()
+  return json.businesses
+}
 
-  /* *********************************************************
-  getResultsLatLon()
-  @param lat (number)
-  @param lon (number)
-  @return array of Yelp businesses with lots of fields!
-  // Example lat/lon for Galvanize"
-  //    latitude=40.016516
-  //    longitude=-105.281656
-  // Addl params sent to Yelp
-  //    sort_by=distance
-  //    radius=1000
-  ************************************************************ */
-  export const getResultsLatLon = async (lat, lon) => {
-    config.params.location = location
-    const response = await fetch(`${API}?term=${config.params.term}&latitude=${latitude}&longitude=${longitude}&sort_by=distance&radius=${RADIUS}`, config)
-    const json = await response.json()
-    return json.businesses
-  }
+/* *********************************************************
+getResultsLatLon()
+@param lat (number)
+@param lon (number)
+@return array of Yelp businesses with lots of fields!
+// Example lat/lon for Galvanize"
+//    latitude=40.016516
+//    longitude=-105.281656
+// Addl params sent to Yelp
+//    sort_by=distance
+//    radius=1000
+************************************************************ */
+export const getResultsLatLon = async (lat, lon) => {
+  config.params.location = location
+  const response = await fetch(`${API}?term=${config.params.term}&latitude=${lat}&longitude=${lon}&sort_by=distance&radius=${RADIUS}`, config)
+  const json = await response.json()
+  return json.businesses
+}
