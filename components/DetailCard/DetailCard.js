9398e9f8c50480aba18a7f8fa5230796385900d5
Ed Phelps <edphelps1@gmail.com>
2018-12-17 08:32:33 -0700

    spacing and comments

--- components/DetailCard/DetailCard.js
+++ components/DetailCard/DetailCard.js
@@ -3,22 +3,21 @@ import {StyleSheet, Dimensions, Text, View, SafeAreaView, ImageBackground, Image
 import store, { URI } from '../../store'
 import { getResults } from '../../utils/api'
 
-
 export default class DetailCard extends React.Component {
-
 
   constructor(props) {
     super(props)
     this.state = {
       locations: store.getState().locations,
       locationForDetail: store.getState().locationForDetail,
+
       isLoading: true,
       error: false,
       matchedLocation: ''
     }
   }
 
-  /* **************************************** Do I need this?*/
+  /* **************************************** */
   async componentDidMount() {
     this.unsubscribe = store.onChange(() => {
       this.setState({
@@ -32,76 +31,78 @@ export default class DetailCard extends React.Component {
     let loadedLocationForDetail = await store.getState().locationForDetail
       // console.log('*******', loadedLocationForDetail)
 
-      if (loadedLocations === undefined) {
-        this.setState({
-          error: true,
-          isLoading: false,
-        });
-        console.log("ERROR DetailCard::componentDidMount()");
-        return;
-      }
+    if (loadedLocations === undefined) {
+      this.setState({
+        error: true,
+        isLoading: false,
+      });
+      console.log("ERROR DetailCard::componentDidMount()");
+      return;
+    }
 
-      if(loadedLocationForDetail === undefined) {
-        this.setState({
-          error: true,
-          isLoading: false,
-        })
-        console.log("ERROR DetailCard::componentDidMount()")
-        return
-      }
+    if (loadedLocationForDetail === undefined) {
+      this.setState({
+        error: true,
+        isLoading: false,
+      });
+      console.log("ERROR DetailCard::componentDidMount()")
+      return
+    }
 
     let matched = loadedLocations.find((location) => (location.id === loadedLocationForDetail))
-    console.log('>>>>>>>>>>>>', matched, matched.name, matched.image_url);
+    console.log('>>>>>>>>>>>', matched, matched.name, matched.image_url);
 
     this.setState({
       matchedLocation: matched
-    })
+    });
     console.log(matchedLocation);
   }
 
+  /* **************************************** */
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
 
-
-
-
+  /* **************************************** */
   render() {
     const { matchedLocation } = this.state
 
     return (
-        <ScrollView style={styles.card}>
-          <View style={styles.imageContainer}>
-            <Image resizeMode={'contain'} style={styles.image} source={{uri: `${matchedLocation.image_url}`}} />
-          </View>
+      <ScrollView style={styles.card}>
+        <View style={styles.imageContainer}>
+          <Image resizeMode={'contain'} style={styles.image} source={{uri: `${matchedLocation.image_url}`}} />
+        </View>
 
-          <View style={styles.cardTopLine}>
-            <Text style={styles.name}>{matchedLocation.name}</Text>
-            <Text style={styles.checkin}>79 Check-ins here!</Text>
-          </View>
+        <View style={styles.cardTopLine}>
+          <Text style={styles.name}>{matchedLocation.name}</Text>
+          <Text style={styles.checkin}>79 Check-ins here!</Text>
+        </View>
 
-          <View style={styles.cardSecondLine}>
-            <Text style={styles.price}>{matchedLocation.price}</Text>
-            <Text style={styles.category}>Categories</Text>
-            <Text style={styles.rating}> ☆☆☆☆☆</Text>
-            <Text style={styles.ratingCount}> (797)</Text>
-          </View>
+        <View style={styles.cardSecondLine}>
+          <Text style={styles.price}>{matchedLocation.price}</Text>
+          <Text style={styles.category}>Categories</Text>
+          <Text style={styles.rating}> ☆☆☆☆☆</Text>
+          <Text style={styles.ratingCount}> (797)</Text>
+        </View>
 
-          <View style={styles.cardThirdLine}>
-            <Text style={styles.cardThirdLine}>address</Text>
-          </View>
+        <View style={styles.cardThirdLine}>
+          <Text style={styles.cardThirdLine}>address</Text>
+        </View>
 
-          <View style={styles.cardFourthLine}>
-            <Text style={styles.directions}>Directions</Text>
-            <Text style={styles.openNow}> Open Now</Text>
-            <Text style={styles.call}> Call</Text>
-          </View>
+        <View style={styles.cardFourthLine}>
+          <Text style={styles.directions}>Directions</Text>
+          <Text style={styles.openNow}> Open Now</Text>
+          <Text style={styles.call}> Call</Text>
+        </View>
 
-          <View style={styles.cardFifthLine}>
-            <Text >Leave a Rating or Comment </Text>
-          </View>
+        <View style={styles.cardFifthLine}>
+          <Text >Leave a Rating or Comment </Text>
+        </View>
 
-          <View style={styles.starRating}>
-            <Text style={styles.starRating}>☆☆☆☆☆</Text>
-          </View>
-        </ScrollView>
+        <View style={styles.starRating}>
+          <Text style={styles.starRating}>☆☆☆☆☆</Text>
+        </View>
+      </ScrollView>
     )
   }
 }
