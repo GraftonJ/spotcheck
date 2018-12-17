552d353888e4b2c79eab19e02c0f445c7e4ea233
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-12-17 14:42:14 -0700

    styling for comments section

--- components/DetailCard/DetailCard.js
+++ components/DetailCard/DetailCard.js
@@ -111,6 +111,11 @@ export default class DetailCard extends React.Component {
           <Text style={styles.starRating}>☆☆☆☆☆</Text>
         </View>
 
+        <View style={styles.ratingsView}>
+          <Text>
+            Spot Check User Ratings!
+          </Text>
+        </View>
 
           {matchedLocation.scComments.map(scComments => (
           <View
@@ -121,7 +126,8 @@ export default class DetailCard extends React.Component {
               style={styles.cardTopLine}>
               <Text
               style={styles.commentName}>Name: {scComments.user.name}</Text>
-              <Text>Dog(s): {scComments.user.dogNames}</Text>
+              <Text
+              style={styles.commentName}>Dog(s): {scComments.user.dogNames}</Text>
             </View>
 
             <View style={styles.cardSecondLine}>
@@ -233,16 +239,20 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   comments: {
-    borderTopWidth: 1,
+    borderBottomWidth: 1,
     marginTop: 15,
   },
   commentName: {
+    flex: 1,
     fontSize: 15,
     fontWeight: 'bold'
   },
   commentContainer: {
     width: "100%",
     height: 200,
-    borderWidth: 3
+    borderTopWidth: 1,
+  },
+  ratingsView: {
+    marginTop: 30,
   }
 })
