fefaafbd1865c6d4b760490b0763866d57c864db
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-12-19 09:34:30 -0700

    pin image aligned with input field

--- components/Homepage/Homepage.js
+++ components/Homepage/Homepage.js
@@ -76,6 +76,18 @@ export default class HomePage extends React.Component {
         <View style={styles.container}>
           <Text style={styles.textStyle}>Spot Check</Text>
           <Text style={styles.smallText}>Pup Friendly Restaurants!</Text>
+
+
+
+          <View style={styles.inputContainer}>
+
+
+            <TouchableOpacity onPress={this.onpressPin}>
+              <Image
+                style={{ width: 25, height: 45, marginBottom: 10, marginLeft: 5,}}
+                source={require('../../assets/images/mapsImage.png')} />
+            </TouchableOpacity>
+
           <TextInput
             autoFocus
             autoCorrect={false}
@@ -87,18 +99,12 @@ export default class HomePage extends React.Component {
             onChangeText={this.onchangeSearchFor}
           />
 
+        </View>
           <TouchableOpacity
               style={styles.button}
               onPress={this.onpressSearch}>
             <Text style={styles.buttonText}>Fetch!</Text>
           </TouchableOpacity>
-
-          <TouchableOpacity onPress={this.onpressPin}>
-            <Image
-              style={{width: 25, height: 45, marginTop: 10}}
-              source={require('../../assets/images/mapsImage.png')} />
-          </TouchableOpacity>
-
         </View>
       </ImageBackground>
 
@@ -124,52 +130,59 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 25,
     marginTop: -8,
+    marginBottom: '68%',
     fontFamily: 'MarkerFelt-Wide',
     color: 'white',
     letterSpacing: 1,
     textDecorationColor: 'white',
     textDecorationLine: 'underline',
-},
-textInput: {
-  backgroundColor: 'rgba(222, 224, 226, 0.8)',
-  borderWidth: 1,
-  borderRadius: 3,
-  color: 'black',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
 
-  width: 300,
-  marginTop: '65%',
-  marginHorizontal: 20,
-  padding: 5,
-  alignSelf: 'center',
-  fontFamily: 'Oxygen',
-  fontSize: 18,
-},
-imageContainer: {
-  flex: 1,
-  width: 450,
-},
-image: {
-  flex: 1,
-  width: null,
-  height: null,
-  opacity: .80,
-  resizeMode: 'cover',
-},
-button: {
-  width: '25%',
-  height: '6%',
-  borderWidth: 1,
-  borderRadius: 10,
-  backgroundColor: 'white',
-  marginTop: 10,
-  alignItems: 'center',
-  justifyContent: 'center',
+  },
+  textInput: {
+    backgroundColor: 'rgba(222, 224, 226, 0.8)',
+    borderWidth: 1,
+    borderRadius: 3,
+    color: 'black',
+    width: 250,
+    // marginTop: '65%',
+    marginHorizontal: 20,
+    padding: 5,
+    alignSelf: 'center',
+    fontFamily: 'Oxygen',
+    fontSize: 18,
+  },
+  imageContainer: {
+    flex: 1,
+    width: 450,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    opacity: .80,
+    resizeMode: 'cover',
+  },
+  button: {
+    width: '25%',
+    height: '6%',
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
 
-},
-buttonText: {
-  fontFamily: 'MarkerFelt-thin',
-  padding: 4,
-  letterSpacing: 1,
-  fontSize: 22,
-}
+  },
+  buttonText: {
+    fontFamily: 'MarkerFelt-thin',
+    padding: 4,
+    letterSpacing: 1,
+    fontSize: 22,
+  }
 });
